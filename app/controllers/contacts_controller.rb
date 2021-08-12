df5984ec893b1a509ad18bc1c15fd66902f2a77e
atsushi oshima <aboutagial@yahoo.co.jp>
2021-08-12 14:19:03 +0900

    form_with完成

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,7 +1,10 @@
 class ContactsController < ApplicationController
-  def new
+  def new   #フォームを作成。DBのレコードに追加。
+    @contact = Contact.new
   end
 
-  def create
+  def create  #newで入力した値を取得
+    Contact.create(name: params[:contact][:name], email:params[:contact][:email], content: params[:contact][:content])
+    
   end
 end
