81ccd438976f09a391f17530966557cce874c9c1
atsushi oshima <aboutagial@yahoo.co.jp>
2021-08-12 17:14:40 +0900

    エラーメッセージ１

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,10 +6,16 @@ class ContactsController < ApplicationController
   def create  #newで入力した値を保存。DBのレコードに追加。#paramsメソッドで、フォームの入力値を取得
     #下に修正→Contact.create(name: params[:contact][:name], email:params[:contact][:email], content: params[:contact][:content])
     #更に修正、メソッドにする→Contact.create(params.require(:contact).permit(:name, :email, :content))
-    Contact.create(contact_params)
-    redirect_to new_contact_path
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      redirect_to new_contact_path, notice: "ブログ作成！"
+    #redirect_to new_contact_path
+    else
+      render :new
+    end
   end
   private   #strong Parameters〜セキュリティ強化
   def contact_params
     params.require(:contact).permit(:name, :email, :content)
+  end
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,3 @@
 class Contact < ApplicationRecord
+    validates :content, presence: true, length: {maximum: 140}
 end
